b4a8d85f24a51858449979cf3692e0cb24f87bbc
hnamazian <namazian87@gmail.com>
2020-05-29 23:53:57 +0430

    Create createTask as POST method to create task on http request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) { }
 
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks()
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title,
+    @Body('description') description): Task {
+    const task = this.tasksService.createTask(title, description)
+    return task
   }
 }
