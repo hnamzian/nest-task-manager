5480d8caa4b8ed490df1cdb841861c4a3d0c3968
hnamazian <namazian87@gmail.com>
2020-05-31 00:56:25 +0430

    Create delete task by id api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -21,5 +21,10 @@ export class TasksController {
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     const task = this.tasksService.createTask(createTaskDto)
     return task
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTaskById(id)
   }
 }
