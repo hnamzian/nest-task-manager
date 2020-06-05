a79558c3e408509c23cd4f7e2d1fa978334683b6
hnamazian <namazian87@gmail.com>
2020-06-05 17:49:36 +0430

    Import TypeOrm module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfig } from './config/typeorm.config';
+// import { TypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(TypeOrmConfig),
+    TypeOrmModule.forRoot(),
     TasksModule],
 })
 export class AppModule {}
