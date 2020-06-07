59a53738931e0c1684351dbb020a074ace41e654
hnamazian <namazian87@gmail.com>
2020-06-07 18:50:53 +0430

    Generate salt and hash password using salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCrentialsDto } from "./dto/auth-crentials.sto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,15 +11,20 @@ export class UserRepository extends Repository<User> {
 
     const user = new User()
     user.username = username
-    user.password = password
+    user.salt = await bcrypt.genSalt()
+    user.password = await this.hashPassword(password, user.salt)
     try {
       await user.save()
-    } catch(ex) {
+    } catch (ex) {
       if (ex.code = '23055')
         throw new ConflictException('Username already exists')
       else
         throw new InternalServerErrorException()
     }
-    
+
+  }
+
+  private async hashPassword(password, salt) {
+    return bcrypt.hash(password, salt)
   }
 }
