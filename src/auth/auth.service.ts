42daea5f74cd7a1f087017b53fe734ce18f764c8
hnamazian <namazian87@gmail.com>
2020-06-07 23:36:53 +0430

    Create signIng method to verify user credentials and return jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthCrentialsDto } from './dto/auth-crentials.sto';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayoad } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
-  ) {}
+    private userRepository: UserRepository,
+    private jwtService: JwtService
+  ) { }
 
   async signUp(authCredentialsDto: AuthCrentialsDto) {
     await this.userRepository.signUp(authCredentialsDto)
+  }
+
+  async signIn(authCredentialsDto: AuthCrentialsDto) {
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto)
+
+    if (!username)
+      throw new UnauthorizedException('Invalid credentials')
+
+
+    const payload: JwtPayoad = { username }
+    const accessToken = await this.jwtService.sign(payload)
+    return { accessToken }
   }
 }
