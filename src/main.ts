e229fd3d34bc40207571f9656bb543e81d6e895d
hnamazian <namazian87@gmail.com>
2020-06-08 09:47:53 +0430

    Use Logger to log out info to console

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TypeOrmConfig } from './config/typeorm.config';
+import { Logger } from '@nestjs/common'
 
 const port = process.env.PORT || 3000
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap')
+
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  console.log(`Server start running on port ${port}`)
+  logger.log(`Server start running on port ${port}`)
 }
 bootstrap();
